680b3a1b06488015a67f166e7fb04ae1b5794020
Kristian Soto <kroyxlab@gmail.com>
2022-10-29 17:56:06 -0400

    Chanjez la nomo dil fina dosiero

--- kreez-dosiero.sh
+++ kreez-dosiero.sh
@@ -10,13 +10,13 @@ if [[ $dosiero == "pdf" ]]; then
 		--toc --toc-depth=2 \
 		--standalone \
 		--pdf-engine=weasyprint \
-		-o dosieri/kustuma-vorti.pdf
+		-o dosieri/mikra-vortaro.pdf
 elif [[ $dosiero == "epub" ]]; then
 	pandoc parti/*.md \
 		--metadata-file=metadata.yaml \
 		--toc --toc-depth=2 \
 		--standalone \
-		-o dosieri/kustuma-vorti.epub
+		-o dosieri/mikra-vortaro.epub
 else
 	exit 1
 fi
